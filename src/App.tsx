98a7d543398524cf355b862c852ba5fa5324489a
Chris Barber-Riley <barberriley@Chriss-MacBook-Pro.local>
2019-09-12 20:20:28 +0100

    TS Render props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Textfield from "./Textfield";
+import Counter from "./counter";
 
 const App: React.FC = () => {
     return (
@@ -10,6 +11,17 @@ const App: React.FC = () => {
                 alive={true}
                 person={{ firstName: "Chris", lastName: "Barber-Riley" }}
             />
+
+            <Counter>
+                {(count, setCount) => (
+                    <div>
+                        {count}{" "}
+                        <button onClick={() => setCount(count + 1)}>
+                            click me
+                        </button>
+                    </div>
+                )}
+            </Counter>
         </div>
     );
 };
--- src/Textfield.tsx
+++ src/Textfield.tsx
@@ -17,12 +17,12 @@ const Textfield: React.FC<Props> = () => {
     // It is automatically defered as default, but we can be explicit in allowing more types
     const [age, setAge] = useState<number | null | string>(31);
 
-    setAge(null); // works as we've declared it
+    // setAge(null); // works as we've declared it (although infinite loop)
     // setAge(undefined); // we haven't declare undefined so it fails
 
     // const [name, setName] = useState<{ name: string }>(31); // fails
     const [name, setName] = useState<{ name: string }>({ name: "Chris" }); // passes
-    setName({ name: "chris" }); // passes
+    // setName({ name: "chris" }); // passes
 
     const inputRef = useRef<HTMLInputElement>(null);
 
