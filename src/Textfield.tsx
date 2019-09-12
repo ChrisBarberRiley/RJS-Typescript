7a7e21656f6b03a7f6bdb6857d797f29b4e05da0
Chris Barber-Riley <barberriley@Chriss-MacBook-Pro.local>
2019-09-12 20:05:10 +0100

    TS Hooks

--- src/Textfield.tsx
+++ src/Textfield.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 interface Person {
     firstName: string;
@@ -14,9 +14,21 @@ interface Props {
 }
 
 const Textfield: React.FC<Props> = () => {
+    // It is automatically defered as default, but we can be explicit in allowing more types
+    const [age, setAge] = useState<number | null | string>(31);
+
+    setAge(null); // works as we've declared it
+    // setAge(undefined); // we haven't declare undefined so it fails
+
+    // const [name, setName] = useState<{ name: string }>(31); // fails
+    const [name, setName] = useState<{ name: string }>({ name: "Chris" }); // passes
+    setName({ name: "chris" }); // passes
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return (
         <div>
-            <input />
+            <input ref={inputRef} />
         </div>
     );
 };
